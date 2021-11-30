b98c7efac570af50ccb36afc9671df79c651b528
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-12-01 02:43:07 +0800

    練習3.1:使用Model查詢資料

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,9 @@ Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
 Route::get('posts/{id}', [PostsController::class, 'show'])->name('posts.show');
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.dashboard.index');
+    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin.dashboard.index');  //進入後台管理介面的路由
 
-    Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');
-    Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');
-    Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
+    Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');  //候台列出所有文章的路由
+    Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');  //候台產生新增表單的路由
+    Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');  //候台生產修改表單的路由
 });
